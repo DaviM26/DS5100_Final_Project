76f8ae6af0d3763437a1a3c3dc9731e675f86774
DaviM26 <100162433+DaviM26@users.noreply.github.com>
2022-07-15 23:06:02 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='MonteCarlo',
+      version='1.0',
+      description='A Monte Carlo simulation with modules to install',
+      author='Davi Meran',
+      author_email='davimeran@gmail.com',
+      url='https://github.com/DaviM26/MonteCarloSimulator',
+      py_modules=['MonteCarloModules']
+     )
